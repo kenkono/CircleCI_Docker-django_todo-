debd5e6e2a7b0290ad724a33864449f910a75bd8
kenkono <kono.k@dynamo-tech.com>
2019-05-31 16:16:03 +0700

    #5 show DB at html

--- src/todo_list/urls.py
+++ src/todo_list/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'app'
+app_name = 'todo_list'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:todo_id>', views.detail, name='detail'),
+    path('new_todo', views.new_todo, name='new_todo'),
 ]
